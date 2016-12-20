ddb1eee5a4065151f90ac3c09150b3f4705b969a
pedius1175 <hiramasa1175@gmail.com>
2016-12-20 19:37:12 +0900

    Input Setting

--- Assets/Hiramasa/Script/hpManager.cs
+++ Assets/Hiramasa/Script/hpManager.cs
@@ -1,8 +1,8 @@
 ﻿
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class hpManager : MonoBehaviour
 {
