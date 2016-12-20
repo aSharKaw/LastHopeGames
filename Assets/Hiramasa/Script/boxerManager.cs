908f611d925a91dfcd3e4601ea6ddc007e153c97
pedius1175 <hiramasa1175@gmail.com>
2016-12-20 20:58:02 +0900

    タルコンガでパンチ！

--- Assets/Hiramasa/Script/boxerManager.cs
+++ Assets/Hiramasa/Script/boxerManager.cs
@@ -5,11 +5,22 @@ using System.Collections.Generic;
 
 public class boxerManager : MonoBehaviour
 {
+  private Animator animator;
+
   void Start()
   {
+    animator = GetComponent<Animator>();
   }
 
   void Update()
   {
+    if (Input.GetButtonDown("TalconghaRight"))
+    {
+      animator.SetBool("Punch", true);
+    }
+    else
+    {
+      animator.SetBool("Punch", false);
+    }
   }
 }
