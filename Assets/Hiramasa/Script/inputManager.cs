8a5a345df8e6d75d1bec503368bcc3f4482c2607
pedius1175 <hiramasa1175@gmail.com>
2017-01-12 01:25:02 +0900

    1P, 2P対応

--- Assets/Hiramasa/Script/inputManager.cs
+++ Assets/Hiramasa/Script/inputManager.cs
@@ -3,52 +3,60 @@ using UnityEngine;
 
 public class inputManager : MonoBehaviour
 {
-  // 1P Input
-  public static bool GetDown1PLeft()
+  public static bool GetDownRight1P()
   {
-    if (Input.GetButtonDown("1PTalconghaLeft") || Input.GetKeyDown(KeyCode.A))
+    if (Input.GetKey(KeyCode.JoystickButton0) ||
+      Input.GetKey(KeyCode.JoystickButton1) ||
+      Input.GetKeyDown(KeyCode.A))
     {
       return true;
     }
     else { return false; }
   }
-  public static bool GetDown1PRight()
+  public static bool GetDownLeft1P()
   {
-    if (Input.GetButtonDown("1PTalconghaRight") || Input.GetKeyDown(KeyCode.D))
+    if (Input.GetKey(KeyCode.JoystickButton2) ||
+      Input.GetKey(KeyCode.JoystickButton3) ||
+      Input.GetKeyDown(KeyCode.D))
     {
       return true;
     }
     else { return false; }
   }
-  public static bool GetDown1PStart()
+  public static bool GetDownStart1P()
   {
-    if (Input.GetButtonDown("1PTalconghaStart") || Input.GetKeyDown(KeyCode.S))
+    if (Input.GetKey(KeyCode.JoystickButton9) ||
+      Input.GetKeyDown(KeyCode.S))
     {
       return true;
     }
     else { return false; }
   }
 
-  // 2P Input 
-  public static bool GetDown2PLeft()
+  public static bool GetDownLeft2P()
   {
-    if (Input.GetButtonDown("2PTalconghaLeft") || Input.GetKeyDown(KeyCode.J))
+    if (Input.GetKey(KeyCode.Joystick1Button0) ||
+      Input.GetKey(KeyCode.Joystick1Button1) ||
+      Input.GetKeyDown(KeyCode.J))
     {
       return true;
     }
     else { return false; }
   }
-  public static bool GetDown2PRight()
+  public static bool GetDownRight2P()
   {
-    if (Input.GetButtonDown("2PTalconghaRight") || Input.GetKeyDown(KeyCode.L))
+    if (Input.GetKey(KeyCode.Joystick1Button2) ||
+      Input.GetKey(KeyCode.Joystick1Button3) ||
+      Input.GetKeyDown(KeyCode.L))
     {
       return true;
     }
     else { return false; }
   }
-  public static bool GetDown2PStart()
+  public static bool GetDownStart2P()
   {
-    if (Input.GetButtonDown("2PTalconghaStart") || Input.GetKeyDown(KeyCode.K))
+    if (Input.GetKey(KeyCode.Joystick1Button9) ||
+      Input.GetKeyDown(KeyCode.K))
     {
       return true;
     }
