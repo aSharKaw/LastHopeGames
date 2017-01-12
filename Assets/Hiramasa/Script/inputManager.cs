9484351e98065fc6cffc8d617a513a8c7b8a4808
pedius1175 <hiramasa1175@gmail.com>
2017-01-12 17:15:32 +0900

    最終調整

--- Assets/Hiramasa/Script/inputManager.cs
+++ Assets/Hiramasa/Script/inputManager.cs
@@ -5,8 +5,8 @@ public class inputManager : MonoBehaviour
 {
   public static bool GetDownRight1P()
   {
-    if (Input.GetKey(KeyCode.JoystickButton0) ||
-      Input.GetKey(KeyCode.JoystickButton1) ||
+    if (Input.GetKeyDown(KeyCode.JoystickButton0) ||
+      Input.GetKeyDown(KeyCode.JoystickButton1) ||
       Input.GetKeyDown(KeyCode.A))
     {
       return true;
@@ -15,8 +15,8 @@ public class inputManager : MonoBehaviour
   }
   public static bool GetDownLeft1P()
   {
-    if (Input.GetKey(KeyCode.JoystickButton2) ||
-      Input.GetKey(KeyCode.JoystickButton3) ||
+    if (Input.GetKeyDown(KeyCode.JoystickButton2) ||
+      Input.GetKeyDown(KeyCode.JoystickButton3) ||
       Input.GetKeyDown(KeyCode.D))
     {
       return true;
@@ -25,7 +25,7 @@ public class inputManager : MonoBehaviour
   }
   public static bool GetDownStart1P()
   {
-    if (Input.GetKey(KeyCode.JoystickButton9) ||
+    if (Input.GetKeyDown(KeyCode.JoystickButton9) ||
       Input.GetKeyDown(KeyCode.S))
     {
       return true;
@@ -35,8 +35,8 @@ public class inputManager : MonoBehaviour
 
   public static bool GetDownLeft2P()
   {
-    if (Input.GetKey(KeyCode.Joystick1Button0) ||
-      Input.GetKey(KeyCode.Joystick1Button1) ||
+    if (Input.GetKeyDown(KeyCode.Joystick1Button0) ||
+      Input.GetKeyDown(KeyCode.Joystick1Button1) ||
       Input.GetKeyDown(KeyCode.J))
     {
       return true;
@@ -45,8 +45,8 @@ public class inputManager : MonoBehaviour
   }
   public static bool GetDownRight2P()
   {
-    if (Input.GetKey(KeyCode.Joystick1Button2) ||
-      Input.GetKey(KeyCode.Joystick1Button3) ||
+    if (Input.GetKeyDown(KeyCode.Joystick1Button2) ||
+      Input.GetKeyDown(KeyCode.Joystick1Button3) ||
       Input.GetKeyDown(KeyCode.L))
     {
       return true;
@@ -55,11 +55,29 @@ public class inputManager : MonoBehaviour
   }
   public static bool GetDownStart2P()
   {
-    if (Input.GetKey(KeyCode.Joystick1Button9) ||
+    if (Input.GetKeyDown(KeyCode.Joystick1Button9) ||
       Input.GetKeyDown(KeyCode.K))
     {
       return true;
     }
     else { return false; }
   }
+
+#if false
+
+  private uint count = 0;
+
+  void Update()
+  {
+    if (GetDownLeft1P()) { Debug.Log("GetDownLeft1P " + count++.ToString()); }
+    if (GetDownRight1P()) { Debug.Log("GetDownRight1P " + count++.ToString()); }
+    if (GetDownStart1P()) { Debug.Log("GetDownStart1P " + count++.ToString()); }
+
+    if (GetDownLeft2P()) { Debug.Log("GetDownLeft2P " + count++.ToString()); }
+    if (GetDownRight2P()) { Debug.Log("GetDownRight2P " + count++.ToString()); }
+    if (GetDownStart2P()) { Debug.Log("GetDownStart2P " + count++.ToString()); }
+  }
+
+#endif
+
 }
