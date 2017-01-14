ecba7278bae27e69a8e2b9ed3b6831722fa26b5e
xSYUxx <syu_neko06@yahoo.co.jp>
2017-01-14 10:49:42 +0900

    演出シーンの修正

--- Assets/Arakawa/Script/EventManager.cs
+++ Assets/Arakawa/Script/EventManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -8,13 +9,31 @@ using UnityEditor;
 
 public class EventManager : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject spown1PPoint;
+    [SerializeField]
     private GameObject defaultLight;
+    [SerializeField]
     private GameObject enterLights;
+    [SerializeField]
     private GameObject battleLights;
+    [SerializeField]
     private GameObject player1Obj, player2Obj;
 
-    [SerializeField, Header("Audio")]
-    private AudioClip Call;
+    //[SerializeField, Header("Audio")]
+    //private AudioClip cheer;
+
+    [SerializeField]
+    private int firstTime;
+
+    //[SerializeField]
+    private int secondTime = 6 * 60;
+
+    [SerializeField]
+    private int thirdTime;
+
+    //[SerializeField]
+    //private int 
 
     /*
     inspector設定データ
@@ -26,28 +45,31 @@ public class EventManager : MonoBehaviour {
     [CustomEditor(typeof(EventManager))]
     public class EventEditer : Editor
     {
-        bool folder = false;
+        private bool _objectFolder = false;
+        private bool _audioFolder = false;
 
         public override void OnInspectorGUI()
         {
-            EventManager editor = target as EventManager;
+            EventManager _editor = target as EventManager;
 
-            if(folder = EditorGUILayout.Foldout(folder, "Objects"))
+            if(_objectFolder = EditorGUILayout.Foldout(_objectFolder, "Objects"))
             {
-                editor.defaultLight = EditorGUILayout.ObjectField("初期ライト", editor.defaultLight, typeof(GameObject), true) as GameObject;
-                editor.enterLights = EditorGUILayout.ObjectField("入場開始時ライト", editor.enterLights, typeof(GameObject), true) as GameObject;
-                editor.battleLights = EditorGUILayout.ObjectField("バトル用ライト", editor.battleLights, typeof(GameObject), true) as GameObject;
-                editor.player1Obj = EditorGUILayout.ObjectField("1Pオブジェクト", editor.player1Obj, typeof(GameObject), true) as GameObject;
-                editor.player2Obj = EditorGUILayout.ObjectField("2Pオブジェクト", editor.player2Obj, typeof(GameObject), true) as GameObject;
+                _editor.defaultLight = EditorGUILayout.ObjectField("初期ライト", _editor.defaultLight, typeof(GameObject), true) as GameObject;
+                _editor.enterLights = EditorGUILayout.ObjectField("入場開始時ライト", _editor.enterLights, typeof(GameObject), true) as GameObject;
+                _editor.battleLights = EditorGUILayout.ObjectField("バトル用ライト", _editor.battleLights, typeof(GameObject), true) as GameObject;
+                _editor.player1Obj = EditorGUILayout.ObjectField("1Pオブジェクト", _editor.player1Obj, typeof(GameObject), true) as GameObject;
+                _editor.player2Obj = EditorGUILayout.ObjectField("2Pオブジェクト", _editor.player2Obj, typeof(GameObject), true) as GameObject;
             }
+
+            //if(_audioFolder = EditorGUILayout.Foldout(_audioFolder, "Audio"))
+            //{
+            //    _editor.cheer = EditorGUILayout.ObjectField("歓声SE", _editor.cheer, typeof(AudioClip), true) as AudioClip;
+            //}
         }
     }
 #endif
 
-
-
     /*
-    ロード？入れるなら、タイトルシーンを弄ること。ここじゃない。
     ロード入れるなら直で遷移、ロード無しならフェードインくらいはほしい。
 
     カメラは２つ。動作は同じ。（マルチウィンドウ用）
@@ -61,23 +83,77 @@ public class EventManager : MonoBehaviour {
     シーン遷移→メイン
     */
 
-    void StartScene()
+    void StartScene(int timeLimit)//明るいとき
     {
+        int _count = 0;
 
+        Instantiate(defaultLight);
+
+        while (_count < timeLimit)
+        {
+            _count++;
+
+            //ガヤガヤ
+        }
     }
 
-    void PlayerEntrance()
+    //int _count = 0;
+    void PlayerEntrance(int timeLimit)//暗転して角にプレイヤー表示
+    {
+        //_count++;
+
+        //if(_count > timeLimit)
+        //{
+        //   // SceneManager.LoadScene("");   
+        //}
+    }
+
+    void BattleStart()//カメラ回転、リング状にプレイヤー表示、遷移
     {
 
     }
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        for (int i = 0; i < secondTime; i++)
+        {
+
+            if (i == secondTime / 4)
+            {
+                Instantiate(enterLights);
+            }
+            else if (i == (secondTime / 2))
+            {
+                Instantiate(battleLights);
+                Instantiate(player1Obj);
+                Instantiate(player2Obj);
+            }
+            else
+            {
+                //Debug.Log("シーン遷移");
+                //SceneManager.LoadScene("");
+            }
+            //else if(_count < (timeLimit / 4 * 3))
+            //{
+
+            //}
+            //else
+            //{
+            //    //
+            //}
+
+        }
+
+    }
+    int _count = 0;
+    // Update is called once per frame
+    void Update () {
+        _count++;
+        if(_count > secondTime)
+        {
+            Debug.Log("シーン遷移");
+            SceneManager.LoadScene("test");
+
+        }
+    }
 }
