543018d0032a59e6f741a3c5955a28d122e33084
xSYUxx <syu_neko06@yahoo.co.jp>
2017-01-14 09:51:03 +0900

    Hasimoto内、同名Scriptか競合してたので修正

--- Assets/Hasimoto/Script/movecamera.cs
+++ Assets/Hasimoto/Script/movecamera.cs
@@ -3,12 +3,14 @@
 public class movecamera : MonoBehaviour
 {
 
-    [SerializeField]
-    float rotatey = 1;
+    float rotate_y = 0.1f;
+    float move_x = 0;
 
     void Update ()
     {
-        transform.Rotate(0, rotatey, 0);
+        move_x += 0.1f;
+        transform.Rotate(0, rotate_y, 0);
+        transform.Translate(100 * Mathf.Sin(move_x), 0, 100 * Mathf.Cos(move_x));
     }
 
 }
