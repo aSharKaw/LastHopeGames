9a38fbb498083bdf380a9b8c183d268051e4ecbf
hiramasa1175 <hiramasa1175@yahoo.co.jp>
2017-01-28 13:18:53 +0900

    countManager add

---
+++ Assets/Main/Script/countManager.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class countManager : MonoBehaviour
+{
+  [SerializeField]
+  private static int count; // 増やすと復帰のときのカンストを減らす
+  public static int getCount => count;
+
+  void Update()
+  {
+    if (hpManager.hp1 == 0.0f)
+    {
+      if (inputManager.GetDownRight1()
+      || inputManager.GetDownLeft1())
+      {
+        count++;
+        Debug.Log($"count1 = {count}");
+      }
+    }
+
+    if (hpManager.hp2 == 0.0f)
+    {
+      if (inputManager.GetDownRight2()
+      || inputManager.GetDownLeft2())
+      {
+        count++;
+        Debug.Log($"count2 = {count}");
+      }
+    }
+  }
+}
