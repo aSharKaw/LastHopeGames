d8be03231f2087ac11a8b66b72f41431c106c5e6
hiramasa1175 <hiramasa1175@yahoo.co.jp>
2017-02-02 13:31:38 +0900

    Texture add Script Update

--- Assets/Main/Script/hpManager.cs
+++ Assets/Main/Script/hpManager.cs
@@ -7,21 +7,20 @@ public class hpManager : MonoBehaviour
   [SerializeField]
   private Image[] hpBar;
 
-  public static float hp1;
-  public static float hp2;
+  public static float hp1 = 1.0f;
+  public static float hp2 = 1.0f;
 
   private float punchDamage = 0.005f;
 
-  void Awake()
-  {
-    hp1 = hpBar[0].fillAmount;
-    hp2 = hpBar[1].fillAmount;
-  }
-
   void Update()
   {
+    // 変数名が長い為、変数に保存
     hp1 = hpBar[0].fillAmount;
     hp2 = hpBar[1].fillAmount;
+
+    // Display1のUI表示をDisplay2にも反映させる
+    hpBar[2].fillAmount = hpBar[1].fillAmount;
+    hpBar[3].fillAmount = hpBar[0].fillAmount;
 
     // 1Pが左パンチ
     if (boxerState.GetBoxerState1().IsName("Base Layer.LeftPunch")
@@ -67,6 +66,7 @@ public class hpManager : MonoBehaviour
       }
     }
     
+    // 復帰
     if(hp1 == 0.0f)
     {
       Return(ref rebornManager.Count, 2, 0, 0.5f);
@@ -79,6 +79,7 @@ public class hpManager : MonoBehaviour
     }
   }
 
+  // 復帰
   void Return(ref int returncount, int limit, int player, float hp)
   {
     if (countManager.Count == 10
