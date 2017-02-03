0d1a55375daed309008a97f1ee20e972a27fb3bb
Shu <syonyan06@gmail.com>
2017-02-04 03:15:29 +0900

    リザルトシーンの作成と、リザルト用のカメラ・マネージャーの仮作成

---
+++ Assets/Admission/Script/CameraAround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAround : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject camera1P, camera2P;
+
+    //[SerializeField]
+    //private int 
+
+    void Start () {
+		
+	}
+
+	void Update () {
+	    	camera1P.transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+    }
+}
---
+++ Assets/Admission/Script/ResultManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultManager : MonoBehaviour {
+    //白フェード！
+    //フラグ受け取る！
+    //勝ったほうにWIN、負けたらLOSE表示！
+    //Display間違えないように！
+    //モーションは仕様書参考！カメラグルグル！
+    //タイムで遷移・・・の前にプレイありがとう画面表示！と同時にプレイ回数カウント！
+    //黒フェードで遷移！
+
+    private float player1HP;
+    private float player2HP;
+
+    private void createResultUI()
+    {
+        //1P Wins
+        if(player1HP > player2HP)
+        {
+            //UIをそれぞれに表示。
+            //カメラは同一動作でいいかも。
+            //ObjectはLayerつけて見せるカメラ調整しようか
+        }
+        //2P Wins
+        else if(player1HP < player2HP)
+        {
+        }
+        //draw
+        else
+        {
+        }
+    }
+
+    void Start ()
+    {	
+	}
+
+	void Update ()
+    {	
+	}
+}
