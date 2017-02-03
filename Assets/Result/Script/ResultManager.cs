f293149cf28d0c07b61783f2a4b284bf3c3b0a27
Shu <syonyan06@gmail.com>
2017-02-04 07:08:32 +0900

    リザルトデータの移動と、カメラ・モーションの作成

---
+++ Assets/Result/Script/ResultManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultManager : MonoBehaviour {
+
+    ResultCamera cameraManager;
+    ResultMothion mothionManager;
+    //白フェード！
+    //フラグ受け取る！
+    //勝ったほうにWIN、負けたらLOSE表示！
+    //Display間違えないように！
+    //モーションは仕様書参考！カメラグルグル！
+    //タイムで遷移・・・の前にプレイありがとう画面表示！と同時にプレイ回数カウント！
+    //黒フェードで遷移！
+
+    //・・・まあフェードじゃなくてそのままメインにくっつけてもいい気がするわ。
+
+    //ここにHP渡せばあとはやってくれるよ
+    private float player1HP = 1;
+    private float player2HP;
+
+    [SerializeField]
+    private int cameraRotateSpeed;
+
+    [SerializeField]
+    private GameObject player1Obj, player2Obj;
+    [SerializeField]
+    private GameObject camera1P, camera2P;
+
+    private int winner;
+    
+
+    void Start ()
+    {
+        cameraManager = GetComponent<ResultCamera>();
+        mothionManager = GetComponent<ResultMothion>();
+
+        if (player1HP > player2HP)
+        {
+            winner = 1;
+        }
+        //2P Wins
+        else if (player1HP < player2HP)
+        {
+            winner = 2;
+        }
+        //draw
+        else
+        {
+            winner = 0;
+        }
+
+        cameraManager.cameraStart(winner, ref camera1P, ref camera2P);
+        mothionManager.mothionStart(winner, ref player1Obj, ref player2Obj);
+    }
+
+    void Update ()
+    {
+        cameraManager.cameraUpdate(cameraRotateSpeed, ref camera1P, ref camera2P);
+	}
+}
---
+++ Assets/Result/Script/ResultMothion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultMothion : MonoBehaviour {
+
+    public void mothionStart (int winner, ref GameObject player1, ref GameObject player2)
+    {
+        if (winner == 1)
+        {
+            player1.GetComponent<Animator>().SetBool("win", true);
+            player2.GetComponent<Animator>().SetBool("lose", true);
+        }
+        else if (winner == 2)
+        {
+            player1.GetComponent<Animator>().SetBool("lose", true);
+            player2.GetComponent<Animator>().SetBool("win", true);
+        }
+        else
+        {
+            player1.GetComponent<Animator>().SetBool("win", true);
+            player2.GetComponent<Animator>().SetBool("win", true);
+        }
+    }
+}
