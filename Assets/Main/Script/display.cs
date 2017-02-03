f870b877e4a3f18ad7ee24ff4e4d0705c1fb1d39
hiramasa1175 <hiramasa1175@yahoo.co.jp>
2017-02-04 02:49:10 +0900

    scriptちょっと修正

--- Assets/Main/Script/display.cs
+++ Assets/Main/Script/display.cs
@@ -13,8 +13,12 @@ public class display : MonoBehaviour
     // Display.displays[0] is the primary, default display and is always ON.
     // Check if additional displays are available and activate each.
     if (Display.displays.Length > 1)
+    {
       Display.displays[1].Activate();
+    }
     if (Display.displays.Length > 2)
+    {
       Display.displays[2].Activate();
+    }
   }
 }
