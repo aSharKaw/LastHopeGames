3652f75227aa9f3e3bce8f958430f2bf15f33a64
Shu <syonyan06@gmail.com>
2017-02-04 16:52:07 +0900

    演出のプレイヤー生成用プログラム仮作成

---
+++ Assets/Admission/Script/CreatePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePlayer : MonoBehaviour {
+
+
+    [SerializeField]
+    private Material player1Skin, player2Skin;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
