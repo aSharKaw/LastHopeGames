02277ed1eff8a8a00e30b1e4ba5dd4bc2965355b
hiramasa1175 <hiramasa1175@yahoo.co.jp>
2017-02-06 18:45:54 +0900

    boxerState 完成

--- Assets/Main/Scripts/boxerState.cs
+++ Assets/Main/Scripts/boxerState.cs
@@ -14,6 +14,11 @@ public class boxerState : MonoBehaviour
   public static bool Idle1, Idle2;
   public static bool Left1, Left2;
   public static bool Right1, Right2;
+  public static bool CounterLeft1, CounterLeft2;
+  public static bool CounterRight1, CounterRight2;
+  public static bool Guard1, Guard2;
+  public static bool Down1, Down2;
+  public static bool Reborn1, Reborn2;
 
   void Start()
   {
@@ -32,5 +37,15 @@ public class boxerState : MonoBehaviour
     Left2 = animState2.IsName("Base Layer.Left");
     Right1 = animState1.IsName("Base Layer.Right");
     Right2 = animState2.IsName("Base Layer.Right");
+    CounterLeft1 = animState1.IsName("Base Layer.CounterLeft");
+    CounterLeft2 = animState2.IsName("Base Layer.CounterLeft");
+    CounterRight1 = animState1.IsName("Base Layer.CounterRight");
+    CounterRight2 = animState2.IsName("Base Layer.CounterRight");
+    Guard1 = animState1.IsName("Base Layer.Guard");
+    Guard2 = animState2.IsName("Base Layer.Guard");
+    Down1 = animState1.IsName("Base Layer.Down");
+    Down2 = animState2.IsName("Base Layer.Down");
+    Reborn1 = animState1.IsName("Base Layer.Reborn");
+    Reborn2 = animState2.IsName("Base Layer.Reborn");
   }
 }
--- Assets/Main/Scripts/hpManager.cs
+++ Assets/Main/Scripts/hpManager.cs
@@ -24,10 +24,34 @@ public class hpManager : MonoBehaviour
     hpBar[2].fillAmount = hpBar[1].fillAmount;
     hpBar[3].fillAmount = hpBar[0].fillAmount;
 
-    // 1Pが左パンチ
-    if (boxerState.Left1 && boxerState.Idle2)
+    Punch(boxerState.Left1, boxerState.Idle2, 1);
+    Punch(boxerState.Left2, boxerState.Idle1, 0);
+    Punch(boxerState.Right1, boxerState.Idle2, 1);
+    Punch(boxerState.Right2, boxerState.Idle1, 0);
+
+    Counter(boxerState.Left1, boxerState.Right1,
+      boxerState.CounterLeft2, boxerState.CounterRight2, 1);
+    Counter(boxerState.Left2, boxerState.Right2,
+      boxerState.CounterLeft1, boxerState.CounterRight1, 0);
+  }
+
+  void Punch(bool state1, bool state2, int player)
+  {
+    if (state1 && state2)
     {
-      hpBar[1].fillAmount -= punchDamage;
+      hpBar[player].fillAmount -= punchDamage;
+    }
+  }
+
+  void Counter(bool state1, bool state2,
+    bool state3, bool state4 ,int player)
+  {
+    if (state1 || state2)
+    {
+      if (state3 || state4)
+      {
+        hpBar[player].fillAmount -= punchDamageCounter;
+      }
     }
   }
 }
