90163559d12c68ec9f2adaa0968fa29f732e6a70
Shu <syonyan06@gmail.com>
2017-02-13 16:14:51 +0900

    Revert "メイン引っこ抜いたぜ！！"
    This reverts commit ea74f0327d11defdbca31eaa82ef64c4723815ee.

---
+++ Assets/Main/Script/boxerInput.cs
@@ -0,0 +1,69 @@
+﻿
+using UnityEngine;
+
+public class boxerInput : MonoBehaviour
+{
+  private Animator anim;
+
+  void Start()
+  {
+    anim = GetComponent<Animator>();
+  }
+  void Update()
+  {
+    // ポーズ中じゃなければ実行
+    if (!pauseManager.getPause())
+    {
+      anim.SetBool("LeftPunch", false);
+      anim.SetBool("RightPunch", false);
+      anim.SetBool("Guard", false);
+      anim.SetBool("Counter", false);
+      anim.SetBool("Down", false);
+      anim.SetBool("Reborn", false);
+
+      // 左パンチ
+      if (inputManager.GetDownLeft1()
+        && !inputManager.GetDownRight1()
+        && !boxerState.GetBoxerState2().IsName("Base Layer.Guard"))
+      {
+        anim.SetBool("LeftPunch", true);
+      }
+      // 右パンチ
+      if (inputManager.GetDownRight1()
+        && !inputManager.GetDownLeft1()
+        && !boxerState.GetBoxerState2().IsName("Base Layer.Guard"))
+      {
+        anim.SetBool("RightPunch", true);
+      }
+      // ガード
+      if (inputManager.GetDownLeft1()
+        && inputManager.GetDownRight1())
+      {
+        anim.SetBool("Guard", true);
+      }
+      // カウンター
+      if (inputManager.GetDownRight1()
+        || inputManager.GetDownLeft1())
+      {
+        if (boxerState.GetBoxerState2().IsName("Base Layer.Guard"))
+        {
+          anim.SetBool("Counter", true);
+        }
+      }
+      // ダウン
+      if (hpManager.hp1 <= 0)
+      {
+                anim.SetBool("Down", true);
+              //  anim.SetTrigger("Down");
+      }
+      // リターン
+      if (countManager.Count >= 10
+        && rebornManager.Count != 0
+        && !boxerState.GetBoxerState1().IsName("Base Layer.Reborn"))
+      {
+        Debug.Log("12gubhjmhgfrghj3");
+        anim.SetBool("Reborn", true);
+      }
+    }
+  }
+}
---
+++ Assets/Main/Script/boxerState.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class boxerState : MonoBehaviour
+{
+  private GameObject boxer1;
+  private GameObject boxer2;
+
+  private Animator boxerAnim1;
+  private Animator boxerAnim2;
+
+  private static AnimatorStateInfo boxerState1;
+  private static AnimatorStateInfo boxerState2;
+
+  // Return
+  public static AnimatorStateInfo GetBoxerState1() { return boxerState1; }
+  public static AnimatorStateInfo GetBoxerState2() { return boxerState2; }
+
+  void Start()
+  {
+    boxer1 = GameObject.Find("Boxer");
+    boxerAnim1 = boxer1.GetComponent<Animator>();
+
+    boxer2 = GameObject.Find("Boxer2");
+    boxerAnim2 = boxer2.GetComponent<Animator>();
+  }
+
+  void Update()
+  {
+    boxerState1 = boxerAnim1.GetCurrentAnimatorStateInfo(0);
+    boxerState2 = boxerAnim2.GetCurrentAnimatorStateInfo(0);
+  }
+}
